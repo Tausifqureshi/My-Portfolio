76ed79520398649e93a6121671a6fa6642ead617
Tausifqureshi <tausifqureshi504@gmail.com>
2023-11-26 21:33:09 +0530

    My-portfolio

---
+++ index.js
@@ -0,0 +1,13 @@
+//NavBar scroll.......
+window.addEventListener('scroll',()=>{
+    let nav = document.querySelector("nav") //Nav tag use
+    nav.classList.toggle("nav-scroll",window.scrollY>0);
+})
+
+// Menu-Bar js.... close and open ;
+let menuBar = document.querySelector(".menu-bar")
+let menuButton = document.querySelector(".menu-button")
+
+menuButton.addEventListener('click',()=>{
+    menuBar.classList.toggle('menu-none')
+})
